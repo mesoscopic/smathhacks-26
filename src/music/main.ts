00d3f7338729237110926de850a57fec8af5877e
Tyler Varnum <tylervarnum9@gmail.com>
2026-03-14 19:25:31 -0400

    did some things

--- src/music/main.ts
+++ src/music/main.ts
@@ -1,9 +1,21 @@
 import * as Tone from "tone";
 
 export function play_music() {
-  const synth = new Tone.Synth().toDestination();
-  const loop = new Tone.Loop(() => {
-    synth.triggerAttackRelease("E2", "3n");
-  }, "2n").start(0);
+  const bass_synth = new Tone.Synth();
+  const distortion = new Tone.Distortion(0.7).toDestination();
+  bass_synth.connect(distortion);
+
+  const treble_synth = new Tone.Synth().toDestination();
+
+  const bass_loop = new Tone.Loop((time) => {
+    bass_synth.triggerAttackRelease("G2", "4n", time);
+  }, "4n").start(0);
+  const treble_loop = new Tone.Loop((time) => {
+    treble_synth.triggerAttackRelease("D#4", "8n", time);
+  }, "4n").start("8n");
+
   Tone.getTransport().start();
+
+  Tone.getTransport().bpm.rampTo(60,1);
+
 }
